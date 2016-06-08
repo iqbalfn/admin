5e3f9fbc9cfa8081d618d0b01ff1430734717c6b
iqbalfn <iqbalfawz@gmail.com>
2016-06-08 15:07:12 +0700

    php echo instead of standar xml

--- theme/robot/feed.php
+++ theme/robot/feed.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?>
+<?= '<?xml version="1.0" ?>' ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <atom:link href="<?= $feed_url ?>" rel="self" type="application/rss+xml" />
