ce572168b2d1235917dc31c6858234de696c5424
iqbalfn <iqbalfawz@gmail.com>
2016-07-18 10:17:52 +0700

    instant article new feature

--- theme/default/post/instant.php
+++ theme/default/post/instant.php
@@ -4,35 +4,38 @@
     <meta charset="utf-8">
     <link rel="canonical" href="<?= base_url($post->page) ?>">
     <meta property="op:markup_version" content="v1.0">
+    <meta property="fb:use_automatic_ad_placement" content="true">
+    <meta property="fb:likes_and_comments" content="enable">
+    <!-- TODO op:tags -->
   </head>
   <body>
     <article>
       <header>
         <h1><?= $post->title->clean(); ?></h1>
+        
         <time class="op-published" datetime="<?= $post->published->format('c') ?>"><?= $post->published->format('M d, Y') ?></time>
         <?php if($post->published->time != $post->updated->time): ?>
         <time class="op-modified" dateTime="<?= $post->updated->format('c') ?>"><?= $post->updated->format('M d, Y') ?></time>
         <?php endif; ?>
         
         <address>
-          <a rel="facebook" href="#"><?= $post->user->fullname ?></a>
-          <?= $post->user->about ?>
+            <a rel="facebook" href="#" title="Reporter"><?= $post->user->fullname ?></a>
         </address>
         
         <figure>
-          <img src="<?= $post->cover ?>" />
-          <?php if($post->cover_label): ?>
-          <figcaption><?= $post->cover_label ?></figcaption>
-          <?php endif; ?>
-        </figure>   
+            <img src="<?= $post->cover ?>" />
+            <?php if($post->cover_label): ?>
+            <figcaption><?= $post->cover_label ?></figcaption>
+            <?php endif; ?>
+        </figure>
+      
+        <figure class="op-ad">
+            <iframe width="320" height="50" style="border:0; margin:0;" src="https://www.facebook.com/adnw_request?placement=1738038036454020_1738038136454010&amp;adtype=banner320x50"></iframe>
+        </figure>
 
       </header>
 
       <?= $post->content ?>
-      
-     <figure class="op-ad">
-        <iframe width="320" height="50" style="border:0; margin:0;" src="https://www.facebook.com/adnw_request?placement=1738038036454020_1738038136454010&amp;adtype=banner320x50"></iframe>
-     </figure>
 
       <figure class="op-tracker">
           <iframe>
